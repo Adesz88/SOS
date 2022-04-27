1b7e016f0d3be2b7156142f25c163f0e8c356ae1
Adam Olej <51245883+Adesz88@users.noreply.github.com>
2022-04-27 19:14:32 +0200

    Working location fetching

--- SOS/EmergencyViewController.swift
+++ SOS/EmergencyViewController.swift
@@ -6,19 +6,33 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class EmergencyViewController: UIViewController {
+class EmergencyViewController: UIViewController{
+    let locationManager = CLLocationManager()
     
     @IBOutlet var Input: UITextField!
-    
     @IBOutlet var Label: UILabel!
+    @IBOutlet var Button: UIButton!
+    @IBOutlet var LocationLabel: UILabel!
     
     var ememergencyNumbers: EmergencyNumbers?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*Button.frame = CGRect(x: 160, y: 100, width: 50, height: 50)
+        Button.layer.cornerRadius = 0.5 * Button.bounds.size.width
+        Button.clipsToBounds = true*/
+        
         ParseJSON()
         print((ememergencyNumbers?.numbers[0].Police.All.first)!!)
+        
+        locationManager.requestWhenInUseAuthorization()
+        if (CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
     }
     
     @IBAction func ButtonPressed(_ sender: UIButton) {
@@ -27,6 +41,11 @@ class EmergencyViewController: UIViewController {
                 Label.text = number.Police.All.first!!
             }
         }
+    }
+    
+    
+    @IBAction func LocationButtonPressed(_ sender: UIButton) {
+        locationManager.startUpdatingLocation()
     }
     
     private func ParseJSON(){
@@ -38,16 +57,30 @@ class EmergencyViewController: UIViewController {
             let jsonData = try Data(contentsOf: url)
             ememergencyNumbers = try JSONDecoder().decode(EmergencyNumbers.self, from: jsonData)
             
-            if let result = ememergencyNumbers{
+            /*if let result = ememergencyNumbers{
                 //print(result)
             } else{
                 print("Failed to parse")
-            }
+            }*/
             return
         } catch{
             print("Error \(error)")
         }
     }
+}
 
-
+extension EmergencyViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationValue: CLLocationCoordinate2D = manager.location?.coordinate else{
+            return
+            
+        }
+        print("locations = \(locationValue.latitude) \(locationValue.longitude)")
+        LocationLabel.text = "\(locationValue.latitude) \(locationValue.longitude)"
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+           print("Failed to find user's location: \(error.localizedDescription)")
+      }
 }
