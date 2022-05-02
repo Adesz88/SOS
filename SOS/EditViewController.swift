c3d57502d7a0be64b05f442e7391dbb5b8be1c9c
Adam Olej <51245883+Adesz88@users.noreply.github.com>
2022-05-02 21:18:28 +0200

    Mostly working settings UI

--- SOS/EditViewController.swift
+++ SOS/EditViewController.swift
@@ -12,6 +12,7 @@ class EditViewController: UIViewController{
     
     
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var TAJTextField: UITextField!
     @IBOutlet weak var birthPlaceTextField: UITextField!
     @IBOutlet weak var birthDatePicker: UIDatePicker!
@@ -19,16 +20,14 @@ class EditViewController: UIViewController{
     @IBOutlet weak var diseasesTextView: UITextView!
     @IBOutlet weak var medicinesTextView: UITextView!
     @IBOutlet weak var SMSTextView: UITextView!
-    @IBOutlet weak var relativeNameTextField: UITextField!
-    @IBOutlet weak var relativePhoneTextField: UITextField!
+    @IBOutlet weak var contactNameTextField: UITextField!
+    @IBOutlet weak var contactPhoneTextField: UITextField!
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var users: [NSManagedObject] = []
 
-    @IBAction func SaveButton(_ sender: UIBarButtonItem) {
-        saveUser()
-        print("szoveg")
-        dismiss(animated: true)
+    override func viewWillAppear(_ animated: Bool) {
+        loadUser()
     }
     
     override func viewDidLoad() {
@@ -36,8 +35,17 @@ class EditViewController: UIViewController{
         // Do any additional setup after loading the view.
     }
     
-    func loadUser(){
-        
+    @IBAction func SaveButton(_ sender: UIBarButtonItem) {
+        if(!users.isEmpty) {
+           editUser()
+        } else {
+            saveUser()
+        }
+        dismiss(animated: true)
+        // TODO: SettingsViewController.loadUser()
+    }
+    
+    func loadUser() {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "User")
         
         do {
@@ -46,34 +54,60 @@ class EditViewController: UIViewController{
             print(error)
         }
         
-        //nameTextField.text = users[0].value(forKeyPath: "name") as? String
-        //TAJTextField.text = users[0].value(forKey: "taj_number") as? String
+        if(!users.isEmpty) {
+            nameTextField.text = users[0].value(forKeyPath: "name") as? String
+            addressTextField.text = users[0].value(forKeyPath: "address") as? String
+            TAJTextField.text = String((users[0].value(forKey: "taj_number") as? Int32 ?? 0))
+            birthPlaceTextField.text = users[0].value(forKeyPath: "birth_place") as? String
+            //birthDatePicker.date
+            //bloodTypePicker
+            diseasesTextView.text = users[0].value(forKeyPath: "diseases") as? String
+            medicinesTextView.text = users[0].value(forKeyPath: "medicines") as? String
+            SMSTextView.text = users[0].value(forKeyPath: "sms_text") as? String
+            contactNameTextField.text = users[0].value(forKeyPath: "contact_name") as? String
+            contactPhoneTextField.text = users[0].value(forKeyPath: "contact_phone") as? String
+            
+        }
     }
     
-    func editUser(){
-        
-        users[0].setValue("user", forKey: "name")
+    func editUser() {
+        users[0].setValue(nameTextField.text, forKey: "name")
+        users[0].setValue(addressTextField.text, forKey: "address")
+        users[0].setValue(Int(TAJTextField.text ?? "0"), forKey: "taj_number")
+        users[0].setValue(birthPlaceTextField.text, forKey: "birth_place")
+        users[0].setValue(birthDatePicker.date, forKey: "birth_date")
+        //users[0].setValue(bloodTypePicker.text, forKey: "blood_type")
+        users[0].setValue(diseasesTextView.text, forKey: "diseases")
+        users[0].setValue(medicinesTextView.text, forKey: "medicines")
+        users[0].setValue(SMSTextView.text, forKey: "sms_text")
+        users[0].setValue(contactNameTextField.text, forKey: "contact_name")
+        users[0].setValue(contactPhoneTextField.text, forKey: "contact_phone")
         
         do {
             try context.save()
-            loadUser()
         } catch let error {
             print(error)
         }
     }
     
-    func saveUser(){
-        
+    func saveUser() {
         let entity = NSEntityDescription.entity(forEntityName: "User", in: context)
-        
         let user = NSManagedObject(entity: entity!, insertInto: context)
         //TODO: mentés
-        user.setValue("Test user", forKey: "name")
-        user.setValue(123456789, forKey: "taj_number")
         
+        user.setValue(nameTextField.text, forKey: "name")
+        user.setValue(addressTextField.text, forKey: "address")
+        user.setValue(Int(TAJTextField.text ?? "0"), forKey: "taj_number")
+        user.setValue(birthPlaceTextField.text, forKey: "birth_place")
+        //user.setValue(birthDatePicker, forKey: "birth_date")
+        //user.setValue(bloodTypePicker.text, forKey: "blood_type")
+        user.setValue(diseasesTextView.text, forKey: "diseases")
+        user.setValue(medicinesTextView.text, forKey: "medicines")
+        user.setValue(SMSTextView.text, forKey: "sms_text")
+        user.setValue(contactNameTextField.text, forKey: "contact_name")
+        user.setValue(contactPhoneTextField.text, forKey: "contact_phone")
         do {
             try context.save()
-            loadUser()
         } catch let error {
             print(error)
         }
--- SOS/SettingsViewController.swift
+++ SOS/SettingsViewController.swift
@@ -11,15 +11,25 @@ import CoreData
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var TAJTextField: UITextField!
+    @IBOutlet weak var birthPlaceTextField: UITextField!
+    @IBOutlet weak var birthDatePicker: UIDatePicker!
+    @IBOutlet weak var bloodTypePicker: UIPickerView!
+    @IBOutlet weak var diseasesTextView: UITextView!
+    @IBOutlet weak var medicinesTextView: UITextView!
+    @IBOutlet weak var SMSTextView: UITextView!
+    @IBOutlet weak var contactNameTextField: UITextField!
+    @IBOutlet weak var contactPhoneTextField: UITextField!
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var users: [NSManagedObject] = []
     
     override func viewWillAppear(_ animated: Bool) {
         loadUser()
-        print(users)
-        print(users[0].value(forKeyPath: "name")!)
+        if(!users.isEmpty){
+            print(users)
+        }
     }
     
     override func viewDidLoad() {
@@ -27,7 +37,11 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func loadUser(){
+    @IBAction func DeleteButtonPressed(_ sender: UIButton) {
+        deleteUser()
+    }
+    
+    func loadUser() {
         
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "User")
         
@@ -37,8 +51,33 @@ class SettingsViewController: UIViewController {
             print(error)
         }
         
-        nameTextField.text = users[0].value(forKeyPath: "name") as? String
-        TAJTextField.text = users[0].value(forKey: "taj_number") as? String
+        if(!users.isEmpty) {
+            nameTextField.text = users[0].value(forKeyPath: "name") as? String
+            addressTextField.text = users[0].value(forKeyPath: "address") as? String
+            TAJTextField.text = String((users[0].value(forKey: "taj_number") as? Int32 ?? 0))
+            birthPlaceTextField.text = users[0].value(forKeyPath: "birth_place") as? String
+            birthDatePicker.date = users[0].value(forKeyPath: "birth_place") as? Date ?? Date.distantFuture
+            //bloodTypePicker
+            diseasesTextView.text = users[0].value(forKeyPath: "diseases") as? String
+            medicinesTextView.text = users[0].value(forKeyPath: "medicines") as? String
+            SMSTextView.text = users[0].value(forKeyPath: "sms_text") as? String
+            contactNameTextField.text = users[0].value(forKeyPath: "contact_name") as? String
+            contactPhoneTextField.text = users[0].value(forKeyPath: "contact_phone") as? String
+        }
+    }
+    
+    func deleteUser() {
+        for userItem in users {
+            context.delete(userItem)
+        }
+        
+        do {
+            try context.save()
+        } catch let error {
+            print(error)
+        }
+        loadUser()
+        // TODO: present(EmergencyViewController(), animated: true)
     }
     
 
